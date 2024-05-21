35efd1bbed9185cd04b454ed3dd75cf7c16f7b27
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-22 00:16:29 +0300

    Added some models

--- app/models/base.py
+++ app/models/base.py
@@ -1,9 +1,14 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        """
+        Вместо того чтобы каждый раз создавать имя таблицы вручную, мы будем делать это автоматически
+        """
+        return f'{cls.__name__.lower()}s'
+
     id: Mapped[int] = mapped_column(primary_key=True)
-
-
--- app/models/product.py
+++ app/models/product.py
@@ -1,5 +1,8 @@
+from sqlalchemy.orm import Mapped
+
 from .base import Base
 
 
 class Product(Base):
-    __tablename__ = 'product'
+    name: Mapped[str]
+    price: Mapped[int]
