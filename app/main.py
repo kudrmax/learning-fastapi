f2e2275c584afea57a179911613590b73ef85c5f
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-21 23:29:22 +0300

    Added Annotated

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+def root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-async def get_item_by_id(item_id: int):
-    return {"item_id": item_id}
+def get_item_by_id(item_id: Annotated[int, Path(ge=1)]):
+    return {
+        'best_name': 'Max',
+        "item_id": item_id
+    }
 
 
 if __name__ == "__main__":
