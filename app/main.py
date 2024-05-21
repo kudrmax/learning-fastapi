55ea874a6d609de70a1a2240752aa95212129ebf
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-21 18:43:10 +0300

    Added get_item_by_id

--- app/main.py
+++ app/main.py
@@ -9,5 +9,10 @@ async def root():
     return {"Hello": "World"}
 
 
+@app.get("/items/{item_id}")
+async def get_item_by_id(item_id: int):
+    return {"item_id": item_id}
+
+
 if __name__ == "__main__":
     uvicorn.run('main:app', reload=True)
