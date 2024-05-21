bb886c102f709f846fe9dfeb14323f1157f64ae5
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-21 18:12:36 +0300

    Added bd

--- app/core/config.py
+++ app/core/config.py
@@ -1,20 +1,25 @@
 from pydantic import BaseModel
+from pydantic import PostgresDsn  # валидация строки подключения к PostgreSQL
 from pydantic.v1 import BaseSettings
 
 
 class RunConfig(BaseModel):
     host: str = 'localhost'
-    port: int = 5432
+    port: int = 5555
 
 
 class ApiPrefix(BaseModel):
     prefix: str = '/api'
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
-    # db_url: str
+    bd: DatabaseConfig
 
 
 settings = Settings()
