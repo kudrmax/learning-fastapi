0645bd363ea19a654f5cb7fc313aa1a7e88480b5
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-22 13:10:20 +0300

    Added get_product

--- app/api/products/crud.py
+++ app/api/products/crud.py
@@ -9,3 +9,5 @@ async def get_products(session: AsyncSession) -> list[Product]:
     result: Result = await session.execute(stmt)
     products = result.scalars().all()
     return list(products)
+async def get_product(session: AsyncSession, product_id: int) -> Product | None:
+    return await session.get(Product, product_id)
--- app/api/products/views.py
+++ app/api/products/views.py
@@ -9,3 +9,12 @@ router = APIRouter(tags=["products"])
 @router.get('/', response_model=list[Product])
 async def get_products(session):
     return await crud.get_products(session=session)
+@router.get('/{product_id}', response_model=Product)
+async def get_product(product_id: int, session):
+    product = await crud.get_product(session=session, product_id=product_id)
+    if product:
+        return product
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Product {product_id} not found",
+    )
