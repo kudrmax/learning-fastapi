884199960ed8be6a8c198854f791ef684dcb3296
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-22 13:10:33 +0300

    Added create_product

--- app/api/products/views.py
+++ app/api/products/views.py
@@ -9,6 +9,13 @@ router = APIRouter(tags=["products"])
 @router.get('/', response_model=list[Product])
 async def get_products(session):
     return await crud.get_products(session=session)
+
+
+@router.post('/', response_model=Product)
+async def create_product(session, product_in: ProductCreate):
+    return await crud.create_product(session=session, product_in=product_in)
+
+
 @router.get('/{product_id}', response_model=Product)
 async def get_product(product_id: int, session):
     product = await crud.get_product(session=session, product_id=product_id)
