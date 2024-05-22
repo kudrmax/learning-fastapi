5deec32863e32347c71db8717622d3158f6abb4a
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-22 13:27:38 +0300

    Added api to main

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ import uvicorn
 
 from fastapi import FastAPI
 
-from models import Base,db_helper
+from core.config import settings
+from models import Base, db_helper
+from api_v1 import router as router_v1
 from items_views import router as items_router
 from users.views import router as users_router
 
@@ -17,6 +19,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 app.include_router(items_router)
 app.include_router(users_router)
 
