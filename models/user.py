457ebe24d08fd7429833c4297c40816c44b31af4
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-22 19:01:52 +0300

    Create __repr__ and __str__ for User

--- models/user.py
+++ models/user.py
@@ -13,4 +13,10 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(40), unique=True)
 
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
-    profile: Mapped['Profile'] = relationship(back_populates="user")
+    profile: Mapped["Profile"] = relationship(back_populates="user")
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}(id={self.id}, username={self.username!r})"
+
+    def __repr__(self) -> str:
+        return str(self)
