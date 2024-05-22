6180e6532c306a40ad913f8cd616519f87023d9e
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-22 13:19:17 +0300

    Added type of session in product/views

--- app/api/products/views.py
+++ app/api/products/views.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from models import db_helper
 from . import crud
 from .schemas import Product, ProductCreate
 
@@ -7,17 +9,25 @@ router = APIRouter(tags=["products"])
 
 
 @router.get('/', response_model=list[Product])
-async def get_products(session):
+async def get_products(
+        session: AsyncSession = Depends(db_helper.session_dependency)
+):
     return await crud.get_products(session=session)
 
 
 @router.post('/', response_model=Product)
-async def create_product(session, product_in: ProductCreate):
+async def create_product(
+        product_in: ProductCreate,
+        session: AsyncSession = Depends(db_helper.session_dependency),
+):
     return await crud.create_product(session=session, product_in=product_in)
 
 
 @router.get('/{product_id}', response_model=Product)
-async def get_product(product_id: int, session):
+async def get_product(
+        product_id: int,
+        session: AsyncSession = Depends(db_helper.session_dependency),
+):
     product = await crud.get_product(session=session, product_id=product_id)
     if product:
         return product
